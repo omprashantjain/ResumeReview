b79b4ebd9eb214cf8dca36e334a8ad604fe2d2e0
Prashant Jain <prashant@knowledgegate.in>
2023-09-24 14:36:11 +0530

    first

---
+++ js/resume.js
@@ -0,0 +1,23 @@
+function posDone() {
+    let elements = document.getElementsByClassName('pos-checkbox');
+    let posList = document.querySelector('#pos-list');
+    posList.innerHTML = '';
+    for(var i=0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            posList.innerHTML = posList.innerHTML 
+            + `<li class="c0 li-bullet-0"><span class="c3">${elements[i].parentElement.innerText}</span></li>`;
+        }
+    }
+}
+
+function negDone() {
+    let elements = document.getElementsByClassName('neg-checkbox');
+    let negList = document.querySelector('#neg-list');
+    negList.innerHTML = '';
+    for(var i=0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            negList.innerHTML = negList.innerHTML 
+            + `<li class="c0 li-bullet-0"><span class="c6">${elements[i].parentElement.innerText}</span></li>`;
+        }
+    }
+}
